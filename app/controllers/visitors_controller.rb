04e82ca27ea1508bc171c3a4f2847777d9d70443
cmarinoss <cmarinos@aldogroup.com>
2016-03-15 23:49:11 +0000

    adding flash messages

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,6 +3,7 @@ class VisitorsController < ApplicationController
   def new
     
     @owner = Owner.new
-    render 'visitors/new'
+    flash[:notice] = 'Welcome!'
+    flash[:alert] = 'My birthday is soon.'
   end
 end
